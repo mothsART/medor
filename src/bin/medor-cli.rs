323e549403ca09a61a070e72d28a2023094d0135
Ferry Jérémie <ferryjeremie@free.fr>
2022-11-20 16:37:03 +0100

    update dependencies

--- src/bin/medor-cli.rs
+++ src/bin/medor-cli.rs
@@ -4,23 +4,29 @@ extern crate desktopd;
 
 use std::env;
 
-use desktopd::db::basic::{DesktopDDb, Db};
+use desktopd::db::basic::{Db, DesktopDDb};
 use desktopd::db::search::SearchDb;
 
 use medor::cli::build_cli;
 
-fn main() {
-    let matches =
-        build_cli(crate_name!(), crate_version!()).get_matches();
-    let locale = env::var("LANG").unwrap();
-    //println!("{}", locale);
-    let search_db = DesktopDDb::new();
-    search_db.get("nav", &locale);
-
-/*
-    for r in results.unwrap() {
-        println!("{:?}", r.0);
+fn get_local() -> String {
+    if let Ok(_l) = env::var("LANG") {
+        return _l;
     }
-*/
-    println!("Hello, world!");
+    "en_EN.UTF-8".to_string()
+}
+
+fn main() {
+    let matches = build_cli(crate_name!(), crate_version!()).get_matches();
+    let mut search_db = DesktopDDb::new();
+
+    if let Some(keyword_search) = matches.get_one::<String>("app") {
+        let results = search_db.get(keyword_search, &get_local());
+
+        println!("{:?}", results);
+        /*
+        for r in results {
+            println!("{:?}", r.0);
+        }*/
+    }
 }
