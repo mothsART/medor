6f3edc5e388125729641376ed0a19e6353df9f4f
Ferry Jérémie <ferryjeremie@free.fr>
2022-11-26 18:54:24 +0100

    WIP : select app to launch

--- src/bin/medor-cli.rs
+++ src/bin/medor-cli.rs
@@ -3,6 +3,8 @@ extern crate clap;
 extern crate desktopd;
 
 use std::env;
+use std::io::stdin;
+use std::process::Command;
 
 use desktopd::models::SearchResult;
 use desktopd::db::basic::{Db, DesktopDDb};
@@ -17,10 +19,71 @@ fn get_local() -> String {
     "en_EN.UTF-8".to_string()
 }
 
-fn choice(results: &Vec<Option<SearchResult>>, len: usize) {
-    println!("nb results : {}", len);
-    for r in results {
-        println!("{:?}", r);
+fn launch_app(result: &SearchResult) {
+    println!("{:?}", result);
+    //if 
+    //Command::new(result.).output();
+}
+
+fn prompt(title: &str, results: &Vec<Option<SearchResult>>, len: usize) {
+    if len == 1 {
+        let first_r = results.first();
+        if let Some(Some(r)) = &first_r {
+            let title = &r.title;
+            let c = r.comment.as_ref();
+            println!(
+                "Launch \"{}\" : {} [yn]?", 
+                &title, &c.unwrap_or(&"".to_string())
+            );
+            let mut input = String::new();
+            let read_input = stdin().read_line(&mut input);
+            if let Ok(input_value) = input.trim().parse::<char>() {
+                if input_value == 'y' {
+                    launch_app(r);
+                } else if input_value != 'n' {
+                    prompt("", results, len);
+                }
+            }
+        } else {
+            println!("No results");
+        }
+        return;
+    }
+    println!("{}", title);
+    let mut inc = 0;
+    for result in results {
+        if let Some(r) = result {
+            let c = r.comment.as_ref();
+            println!("{}) {} :  \"{}\"", inc, r.title, &c.unwrap_or(&"".to_string()));
+            inc += 1;
+        }
+    }
+    let mut input = String::new();
+    let read_input = stdin().read_line(&mut input);
+    
+    if let Ok(input_value) = input.trim().parse::<usize>() {
+        //if let Some(trim_value) = input_value.trim() {
+        println!("yolo {}", input_value);
+        //}
+        /*match i.trim().parse::<usize>() {
+            Ok(value) if value > 0 && value <= len => {
+                /*let program = kill_list.get(value - 1).unwrap();
+                let killall_cmd = Command::new("killall").arg(program).output();
+                match killall_cmd {
+                    Ok(_v) => {
+                        println!("\"{}\" has been killed", program);
+                    }
+                    Err(_e) => {
+                        eprintln!("{} did'nt succeed in killing {}", crate_name!(), program);
+                    }
+                }
+                */
+            }
+            Ok(input) => prompt(&format!("Wrong input \"{}\". Retry :", input), results),
+            Err(_e) => prompt(&format!("Wrong input \"{}\". Retry :", input), results),
+        };*/
+    } else {
+        return prompt("Wrong input \"a\". Retry :", results, len);
     }
 }
 
@@ -37,10 +100,12 @@ fn main() {
         let results = search_db.get(keyword_search, &get_local(), 10);
 
         if let Ok(ref r) = results {
-            if r.len() == 0 {
+            let len = r.len();
+            if len == 0 {
                 println!("No results");
             } else {
-                choice(r, r.len());
+                println!("{}", len);
+                prompt("Choose application to launch :", r, len);
             }
         } else {
             println!("No results");
